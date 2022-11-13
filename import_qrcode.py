dedb3480d04ae405df3997c740bdeea1a41e20c6
Zo Abbasi <102759010+Zapak23@users.noreply.github.com>
2022-11-13 01:53:54 -0600

    Add files via upload

---
+++ import_qrcode.py
@@ -0,0 +1,9 @@
+import qrcode
+
+qr = qrcode.QRCode(version=1,box_size=10, border=4)
+
+qr.add_data('https://www.ringcentral.com/')qr.make(fit=True)
+
+img = qr.make_image(fill_color='black', back_color='white')
+
+img.save('https://www.ringcentral.com/')
